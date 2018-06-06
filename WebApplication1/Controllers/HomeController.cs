210722d426cab29ccda8cbad38e08b175316eade
shortojke <a.m.klyuchko@gmail.com>
2018-06-06 23:12:15 +0300

    Testtest1233

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace WebApplication1.Controllers
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-            //test changes
+            //test changes1
             return View();
         }
 

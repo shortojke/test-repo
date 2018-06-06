fcddbe9ef3f82ca4a85f764e6e416362436cf57d
shortojke <a.m.klyuchko@gmail.com>
2018-06-06 23:14:53 +0300

    4

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace WebApplication1.Controllers
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-            //test changes1
+            //test changes123
             return View();
         }
 

af826047c71b99e346839a2b0de5fd69a41cc28d
shortojke <a.m.klyuchko@gmail.com>
2018-06-06 23:19:37 +0300

    test

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -5,6 +5,9 @@ namespace WebApplication1.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        /// <summary>
+        /// test
+        /// </summary>
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
